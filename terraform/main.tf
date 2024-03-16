a8c96ed16965da251657c4ce5a811e4d58764c19
lukit <lukkitaalves6@gmail.com>
2024-03-15 21:07:48 -0300

    feat: Ajuste 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-  required_version = ">= 0.15.0"
-}
-
 provider "aws" {
   region = "sa-east-1"
 }
